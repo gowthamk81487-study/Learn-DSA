383905c9d6d39f2ea765757d57cf2a1e7481b665
gowthamk81487-study <gowthamk81487@gmail.com>
2026-09-11 10:11:01 +0530

    Day 1 FN Topic 1 : Maths & Combinatronics

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 class Math{
     public int gcd(int a, int b){
         int r;
@@ -16,6 +17,15 @@ class Math{
     public int nsum(int n){
         return (n*(n+1))/2;
     }
+    public void isprime(int n){
+        for(int i = 2;i<=n/2;i++){
+            if(n%i==0){
+                System.out.println(n+" is not a prime");
+                return;
+            }
+        }
+        System.out.println(n+" is a prime");
+    }
 
 }
 public class Main{
@@ -27,5 +37,6 @@ public class Main{
     System.out.println("GCD of "+a+" and "+b+" is: "+m.gcd(a,b));
     System.out.println(a+" is even: "+m.oe(a));
     System.out.println("Sum of first "+a+" natural numbers is: "+m.nsum(a));
+    m.isprime(a);
 }
 }
