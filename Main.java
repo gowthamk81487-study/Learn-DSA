72efd9dd8e193d72b3598ad7d143b857ee6b50a4
gowthamk81487-study <gowthamk81487@gmail.com>
2026-09-23 14:04:48 +0530

    fsactorial and combinations

--- Main.java
+++ Main.java
@@ -36,16 +36,25 @@ class Maths{
         }
         return false;
     }
-    public double sqroot(int n){
-        double sr=0;
-        for(double i=1;i<=n/2.0;i=i+0.001){
-            if(Math.abs(i*i-n)<0.01){
-                sr = (double)i;
-                break;
-            }
+    public int fact(int n){
+        int f=1;
+        while ((n>0)) {
+            f*=n;
+            n--;
+            
         }
-        return sr;
-}
+        return f;
+    }
+    double comb(int n, int r){
+        if(n>r){
+        double num= fact(n),de= (fact(r)*fact(n-r));
+        return num/de;
+        }
+        else{
+            System.out.print("Invalid Input: "+n+" is greater than "+r);
+            return 0;
+        }
+    }
 }
 public class Main{
     public static void main(String args[]){
@@ -53,13 +62,19 @@ public class Main{
     Maths m = new Maths();
     int a = sc.nextInt();
     int b = sc.nextInt();
+    System.out.println("Factorial of "+" is: "+m.fact(a));
+    if(m.comb(a,b)!=0){
+        System.out.println(a+"C"+b+" = "+m.comb(a,b));
+    }
+    else{
+        m.comb(a,b);
+    }
     System.out.println("GCD of "+a+" and "+b+" is: "+m.gcd(a,b));
     System.out.println("LCM of "+a+" and "+b+" is: "+m.lcm(a,b));
     int k = sc.nextInt();
     System.out.println(k+" is even: "+m.oe(k));
     System.out.println("Sum of first "+k+" natural numbers is: "+m.nsum(k));
     m.isprime(k);
-    System.out.println("Square root of "+k+" is: "+m.sqroot(k));
     String n = sc.next();
     System.out.println(n+" is divisible by 13: "+m.isdiv13(n));
 }
